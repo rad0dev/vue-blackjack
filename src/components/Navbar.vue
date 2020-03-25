7246c7932e8ce77e5641b7fa3a15117738ffcada
radoslawolejniczak <r.olejniczak@asper.net.pl>
2020-03-25 20:51:22 +0100

    buefy + initial style & structure

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,53 @@
 <template>
-
+  <b-navbar transparent="true">
+    <template slot="brand">
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <img
+          class="logo"
+          src="../assets/img/blackjack.svg"
+          alt="VueJs BlackJack Card Game"
+        >
+      </b-navbar-item>
+    </template>
+    <template slot="start">
+      <b-navbar-item
+        tag="router-link"
+        :to="{ path: '/' }">
+        Home
+      </b-navbar-item>
+      <b-navbar-item
+        tag="router-link"
+        :to="{ path: '/game' }">
+        Game
+      </b-navbar-item>
+    </template>
+  </b-navbar>
 </template>
 
 <script>
-  export default {
-    name: 'Navbar'
-  }
+export default {
+  name: 'Navbar'
+}
 </script>
 
-<style scoped>
+<style lang="scss" scoped >
+  .logo {
+    height: 100%;
+    width: 100%;
+  }
 
+  .navbar {
+    background: transparent;
+  }
+
+  .navbar-item,
+  .navbar-link {
+    color: #fff;
+
+    &:hover,
+    &:active,
+    &:focus {
+      color: #ddd;
+    }
+  }
 </style>
