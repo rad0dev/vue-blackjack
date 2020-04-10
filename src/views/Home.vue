ff45625778b1965a414b7c946dc6ef91be97dd8a
radoslawolejniczak <r.olejniczak@asper.net.pl>
2020-04-10 11:30:50 +0200

    homepage style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="container">
-    <div class="page-container">
+    <div class="homepage page-container">
       <router-link
         class="button is-success is-large"
         :to="{ path: '/game' }"
       >
         Play game
       </router-link>
-      <br>
       <router-link
         class="button is-success is-large"
         :to="{ path: '/highscores' }"
